31e2c8d9f8f9230ed018c27b8ca804239d5aa0b8
Muhammad Alif Darmamulia <59762698+alif338@users.noreply.github.com>
2022-05-13 23:50:58 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      React⚛️ + Vite⚡ + Replit🌀
+    </main>
+  );
+}
+
+export default App;
